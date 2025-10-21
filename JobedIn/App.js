c8bfad151e298e1d53a15c7e82db151e1dc7ea41
Duje Spajic <dujkanmali@gmail.com>
2025-10-21 11:05:52 +0200

    fix(navigation) fixed issue with navigation deps

--- JobedIn/App.js
+++ JobedIn/App.js
@@ -1,12 +1,23 @@
-// App.js - nastavi normalno
 import React from 'react';
 import {View, Text} from 'react-native';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import HomeScreen from './screens/HomeScreen';
+
+import { NavigationContainer } from '@react-navigation/native';
+import AuthNavigator from './Navigation/AuthNavigator';
+
+// Enable react-native-screens MUST FOR IOS!!!!!!!!!!!!!!!!!!!!!!!!
+import { enableScreens } from 'react-native-screens';
+enableScreens();
+
 function App() {
   return (
-    <LoginScreen/>
+    // <LoginScreen/>
+    < NavigationContainer>
+      <AuthNavigator/>
+
+    </NavigationContainer>
     // <RegisterScreen/>
     // <HomeScreen/>
   );
---
+++ JobedIn/Navigation/AuthNavigator.js
@@ -0,0 +1,22 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import LoginScreen from '../screens/LoginScreen'
+import HomeScreen from '../screens/HomeScreen'
+import RegisterScreen from '../screens/RegisterScreen'
+
+
+const Stack = createNativeStackNavigator();
+
+export default function AuthNavigator(){
+   return(
+      <Stack.Navigator
+      screenOptions={{
+         headerShown: false  
+       }}
+     >
+         <Stack.Screen name="Login" component={LoginScreen}/>
+         <Stack.Screen name="Register" component={RegisterScreen}/>
+      </Stack.Navigator>
+   );
+}
